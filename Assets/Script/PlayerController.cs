90319c2ad6bfd477845fa58b1f7a7204d0694d67
rhm0202 <83155164+rhm0202@users.noreply.github.com>
2025-07-14 23:08:20 +0900

    1강
    기본 캐릭터 움직임

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+
+    [SerializeField]
+    private Camera theCamera;
+    private Rigidbody myRigid;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CameraRotation();
+        CharacterRotation();
+    }
+
+    private void CharacterRotation() // 좌우 캐릭터 회전 함수
+    {
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+        Debug.Log(myRigid.rotation);
+        Debug.Log(myRigid.rotation.eulerAngles);
+    }
+
+    private void CameraRotation() // 상하 카메라 회전 시키는 함수
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y"); // 위아래 고개
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+
+    }
+
+    private void Move() // 캐릭터 이동 함수
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal"); // 정면 이동
+        float _moveDirZ = Input.GetAxisRaw("Vertical"); // 앞뒤 이동
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+
+    }
+}
